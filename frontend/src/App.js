e2f844d19099e36682f0c5e8a0df553b25c2f162
vanutcho123 <hainguyen.061001@gmail.com>
2022-10-14 15:49:55 +0700

    2 - Data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import data from "./data";
+import Iframe from "react-iframe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.products.map((item, index) => (
+        <div>
+          <p>{item.index}</p>
+          <h2>{item.name}</h2>
+          <img src={item.image} alt={item.imageCaption} />
+          <Iframe
+            url={item.mapUrl}
+            width="640px"
+            height="320px"
+            id="map"
+            className="map"
+            display="block"
+            position="relative"
+            allowFullScreen="true"
+          />
+        </div>
+      ))}
     </div>
   );
 }
